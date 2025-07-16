9ecb039158d1aa22e64631ceff415d07a4694894
kallul <gogoikallul33@gmail.com>
2025-07-16 09:44:22 +0530

    cross plateform

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -88,7 +88,7 @@ async function login(req, res) {
             httpOnly: true, // JS can't access this cookie (more secure)
             secure: process.env.NODE_ENV === "production", // send over HTTPS only in production
             maxAge: 24 * 60 * 60 * 1000, // 1 day
-            sameSite: "Lax", // CSRF protection
+            sameSite: "None", // CSRF protection
           })
           .status(200)
           .json({
